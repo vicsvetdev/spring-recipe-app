e9ea6a2a70ab5f53ac67b8a7b6d9a356f02b179b
Victor Svetuskin <vicsvetdev@gmail.com>
2021-03-15 12:02:32 +0200

    Created Category entity. Established many-to-many relations with JoinTables between Recipe and Category.

---
+++ src/main/java/com/vicsvetdev/springrecipeapp/model/Category.java
@@ -0,0 +1,41 @@
+package com.vicsvetdev.springrecipeapp.model;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
