8b6633c0a4f293d3fe4f09ded44c775c90055cb3
Victor Svetuskin <vicsvetdev@gmail.com>
2021-03-24 17:13:40 +0200

    Adding Exception message to 404error form.

--- src/main/java/com/vicsvetdev/springrecipeapp/controllers/RecipeController.java
+++ src/main/java/com/vicsvetdev/springrecipeapp/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package com.vicsvetdev.springrecipeapp.controllers;
 
 import com.vicsvetdev.springrecipeapp.commands.RecipeCommand;
+import com.vicsvetdev.springrecipeapp.exceptions.NotFoundException;
 import com.vicsvetdev.springrecipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -48,6 +51,19 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(Exception exception) {
+        log.error("Handling not found exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", exception);
+
+        return modelAndView;
     }
 
 }
