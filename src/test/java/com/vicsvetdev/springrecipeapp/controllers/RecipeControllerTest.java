022ef0e277ecd0c609570c34cab539335858ba9a
Victor Svetuskin <vicsvetdev@gmail.com>
2021-03-24 17:27:40 +0200

    Adding Number Format Exception handler.

--- src/test/java/com/vicsvetdev/springrecipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/com/vicsvetdev/springrecipeapp/controllers/RecipeControllerTest.java
@@ -50,12 +50,19 @@ public class RecipeControllerTest {
     }
 
     @Test
-    public void getRecipeNotFoundTest() throws Exception {
+    public void getRecipeNotFoundExceptionTest() throws Exception {
         when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
 
         mockMvc.perform(get("/recipe/1/show"))
                 .andExpect(status().isNotFound())
                 .andExpect(view().name("404error"));
+    }
+
+    @Test
+    public void getRecipeNumberFormatExceptionTest() throws Exception {
+       mockMvc.perform(get("/recipe/asdf/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
     }
 
     @Test
