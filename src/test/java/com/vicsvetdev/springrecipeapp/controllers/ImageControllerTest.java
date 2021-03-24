c08b2f6a9dd997479e71d8ad2acedc750abd9fc1
Victor Svetuskin <vicsvetdev@gmail.com>
2021-03-24 17:40:35 +0200

    Adding ControllerExceptionHandler.

--- src/test/java/com/vicsvetdev/springrecipeapp/controllers/ImageControllerTest.java
+++ src/test/java/com/vicsvetdev/springrecipeapp/controllers/ImageControllerTest.java
@@ -37,7 +37,9 @@ public class ImageControllerTest {
         MockitoAnnotations.initMocks(this);
 
         controller = new ImageController(imageService, recipeService);
-        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(controller)
+                .setControllerAdvice(new ControllerExceptionHandler())
+                .build();
     }
 
     @Test
@@ -53,6 +55,14 @@ public class ImageControllerTest {
 
         verify(recipeService, times(1)).findCommandById(anyLong());
     }
+
+    @Test
+    public void getImageNumberFormatExceptionTest() throws Exception {
+        mockMvc.perform(get("/recipe/asdf/recipeimage"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+    }
+
 
     @Test
     public void handleImagePostTest() throws Exception{
